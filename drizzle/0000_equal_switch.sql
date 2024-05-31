4d687b7f63080a0b4c5804e7905c85f2d2a012dd
wjx0820 <1989wjx@gmail.com>
2024-05-31 15:08:52 +0800

    update drizzle-kit

--- drizzle/0000_equal_switch.sql
+++ drizzle/0000_equal_switch.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS "accounts" (
 	"name" text NOT NULL,
 	"user_id" text NOT NULL
 );
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "update" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text
+);
