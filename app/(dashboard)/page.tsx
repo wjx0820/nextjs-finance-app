408aa792a17075e5af4694ca73176ba57a34da81
wjx0820 <1989wjx@gmail.com>
2024-06-12 16:09:42 +0800

    features: dashboard chart

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,11 @@
 import { DataGrid } from "@/components/data-grid";
-// import { DataCharts } from "@/components/data-charts";
+import { DataCharts } from "@/components/data-charts";
 
 export default function DashboardPage() {
   return (
     <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
       <DataGrid />
-      {/* <DataCharts /> */}
+      <DataCharts />
     </div>
   );
 }
