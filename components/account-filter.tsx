a79af9c5cf673544a2508440f47d2d673e43089d
wjx0820 <1989wjx@gmail.com>
2024-06-13 10:20:33 +0800

    features: filters

---
+++ components/account-filter.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import qs from "query-string";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const AccountFilter = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const params = useSearchParams();
+  const accountId = params.get("accountId") || "all";
+  const from = params.get("from") || "";
+  const to = params.get("to") || "";
+
+  const { isLoading: isLoadingSummary } = useGetSummary();
+  const { data: accounts, isLoading: isLoadingAccounts } = useGetAccounts();
+
+  const onChange = (newValue: string) => {
+    const query = {
+      accountId: newValue,
+      from,
+      to,
+    };
+
+    if (newValue === "all") {
+      query.accountId = "";
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query,
+      },
+      { skipNull: true, skipEmptyString: true },
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <Select
+      value={accountId}
+      onValueChange={onChange}
+      disabled={isLoadingAccounts || isLoadingSummary}
+    >
+      <SelectTrigger className="h-9 w-full rounded-md border-none bg-white/10 px-3 font-normal text-white outline-none transition hover:bg-white/20 hover:text-white focus:bg-white/30 focus:ring-transparent focus:ring-offset-0 lg:w-auto">
+        <SelectValue placeholder="Select account" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="all">All accounts</SelectItem>
+        {accounts?.map((account) => (
+          <SelectItem key={account.id} value={account.id}>
+            {account.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
---
+++ components/filters.tsx
@@ -0,0 +1,11 @@
+import { DateFilter } from "@/components/date-filter";
+import { AccountFilter } from "@/components/account-filter";
+
+export const Filters = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-2 lg:flex-row lg:gap-x-2 lg:gap-y-0">
+      <AccountFilter />
+      <DateFilter />
+    </div>
+  );
+};
