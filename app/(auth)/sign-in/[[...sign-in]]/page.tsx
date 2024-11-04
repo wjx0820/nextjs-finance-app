9b8898a47f8c9ee71f28251d270cd4465e1d78f4
wjx0820 <1989wjx@gmail.com>
2024-11-04 17:06:11 +0800

    add preset login info

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,15 +4,17 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-      <div className="h-full lg:flex flex-col items-center justify-center px-4">
-        <div className="text-center space-y-4 pt-16">
-          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back!</h1>
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2E2A47]">Welcome Back!</h1>
           <p className="text-base text-[#7E8CA0]">
             Login or Create account to get back to your dashboard!
           </p>
+          <p className="text-base text-[#7E8CA0]">Email: Test@test.com</p>
+          <p className="text-base text-[#7E8CA0]">Password: 12345678</p>
         </div>
-        <div className="flex items-center justify-center mt-8">
+        <div className="mt-8 flex items-center justify-center">
           <ClerkLoaded>
             <SignIn />
           </ClerkLoaded>
@@ -21,7 +23,7 @@ export default function Page() {
           </ClerkLoading>
         </div>
       </div>
-      <div className="h-full bg-sky-600 hidden lg:flex items-center justify-center">
+      <div className="hidden h-full items-center justify-center bg-sky-600 lg:flex">
         <Image src="/logo.svg" height={100} width={100} alt="logo" />
       </div>
     </div>
